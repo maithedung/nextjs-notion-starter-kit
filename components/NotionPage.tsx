0f97e904426cf5680fab74dc07c9a09968873589
maithedung <44361757+maithedung@users.noreply.github.com>
2022-11-07 00:59:49 +0700

    update

--- components/NotionPage.tsx
+++ components/NotionPage.tsx
@@ -246,13 +246,13 @@ export const NotionPage: React.FC<types.PageProps> = ({
   const title =
     tagsPage && propertyToFilterName ? `${propertyToFilterName} ${name}` : name
 
-  console.log('notion page', {
-    isDev: config.isDev,
-    title,
-    pageId,
-    rootNotionPageId: site.rootNotionPageId,
-    recordMap
-  })
+  // console.log('notion page', {
+  //   isDev: config.isDev,
+  //   title,
+  //   pageId,
+  //   rootNotionPageId: site.rootNotionPageId,
+  //   recordMap
+  // })
 
   if (!config.isServer) {
     // add important objects to the window global for easy debugging
@@ -319,7 +319,7 @@ export const NotionPage: React.FC<types.PageProps> = ({
         pageCover={pageCover}
       />
 
-      <GitHubShareButton />
+      {/*<GitHubShareButton />*/}
     </>
   )
 }
